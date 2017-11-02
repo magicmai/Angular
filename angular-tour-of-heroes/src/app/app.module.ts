f4f0a28e2db465ce3dfada3d09749350da4a3778
magicmai <maijing_wy@163.com>
2017-11-02 21:55:26 +0800

    实现基本功能

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,23 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { NgModule }            from '@angular/core';
+import { BrowserModule }       from '@angular/platform-browser';
+import { FormsModule }         from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule }    from './app-routing.module';
+
+import { AppComponent }        from './app.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroService } from './hero.service';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
-  ],
-  declarations: [
-    AppComponent,
-    HeroDetailComponent,
-    HeroesComponent
-  ],
-  providers: [HeroService],
-  bootstrap: [AppComponent]
+	imports: [
+	    BrowserModule,
+	    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+	    AppRoutingModule
+	],
+	declarations: [
+	    AppComponent,
+	    DashboardComponent,
+	    HeroDetailComponent,
+	    HeroesComponent
+	],
+	providers: [HeroService],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
